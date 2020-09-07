4f3c32f3033ccb3a21f7a1d88dc9cd5294909579
ywx <542708753@qq.com>
2020-09-07 23:59:39 +0800

    使用logback配置

--- src/test/java/netty/client/NettyClient1Test.java
+++ src/test/java/netty/client/NettyClient1Test.java
@@ -1,12 +1,16 @@
 package netty.client;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class NettyClient1Test {
+    private static final Logger log = LoggerFactory.getLogger(NettyClient1Test.class);
 
     @Test
     public void Client() {
-        NettyClient nettyClient = new NettyClient("127.0.0.1", 8090);
-        nettyClient.start();
+        log.info("test");
+//        NettyClient nettyClient = new NettyClient("127.0.0.1", 8090);
+//        nettyClient.start();
     }
 }
